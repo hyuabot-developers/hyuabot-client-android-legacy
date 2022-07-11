98a0cc7032607218ccd195c6d1e2320a931006d8
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-07-11 18:39:30 +0900

    feat: 학식 메뉴 및 식당의 위치 팝업 구현 (#7)
    * feat: 학식 메뉴 및 식당의 위치 팝업 구현
    
    * feat: 학식 식당 팝업이 계속 열리는 문제 수정

--- app/src/main/java/app/kobuggi/hyuabot/ui/main/cafeteria/CafeteriaFragment.kt
+++ app/src/main/java/app/kobuggi/hyuabot/ui/main/cafeteria/CafeteriaFragment.kt
@@ -1,16 +1,19 @@
 package app.kobuggi.hyuabot.ui.main.cafeteria
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import app.kobuggi.hyuabot.databinding.FragmentCafeteriaBinding
+import app.kobuggi.hyuabot.utils.Event
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class CafeteriaFragment : Fragment() {
+class CafeteriaFragment : Fragment(), DialogInterface.OnDismissListener {
     private val vm by viewModels<CafeteriaViewModel>()
     private lateinit var binding: FragmentCafeteriaBinding
 
@@ -22,6 +25,35 @@ class CafeteriaFragment : Fragment() {
         binding = FragmentCafeteriaBinding.inflate(inflater, container, false)
         binding.lifecycleOwner = this
         binding.vm = vm
+
+        val cafeteriaListAdapter = CafeteriaListAdapter(requireContext(), arrayListOf()){
+            location, title -> vm.clickCafeteriaLocation(location, title)
+        }
+        binding.cafeteriaMenuList.adapter = cafeteriaListAdapter
+        binding.cafeteriaMenuList.layoutManager = LinearLayoutManager(context)
+
+        vm.fetchData()
+        vm.cafeteriaMenu.observe(viewLifecycleOwner){
+            cafeteriaListAdapter.setCafeteriaList(it)
+        }
+
+        vm.showCafeteriaLocationDialog.observe(viewLifecycleOwner) {
+            if(it.peekContent() && vm.cafeteriaLocation.value != null && vm.cafeteriaName.value != null) {
+                val dialog = CafeteriaLocationDialog(vm.cafeteriaLocation.value!!, vm.cafeteriaName.value!!)
+                dialog.show(requireActivity().supportFragmentManager, "cafeteria_location_dialog")
+                requireActivity().supportFragmentManager.executePendingTransactions()
+            }
+        }
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        vm.showCafeteriaLocationDialog.value = Event(false)
+    }
+
+    override fun onDismiss(p0: DialogInterface?) {
+        vm.showCafeteriaLocationDialog.value = Event(false)
+        vm.fetchData()
     }
 }
