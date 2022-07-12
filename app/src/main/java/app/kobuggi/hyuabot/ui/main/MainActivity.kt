5f777ee2ed1b15a0bb5ecaff003ea7272f1a6880
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-07-12 17:34:34 +0900

    feat: 하단 메뉴를 눌렀을 때 추가적인 버튼이 나오도록 구현

--- app/src/main/java/app/kobuggi/hyuabot/ui/main/MainActivity.kt
+++ app/src/main/java/app/kobuggi/hyuabot/ui/main/MainActivity.kt
@@ -22,6 +22,28 @@ class MainActivity : GlobalActivity() {
 
         val navigationHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
         val navController = navigationHostFragment.navController
+
         binding.bottomNavigationMenu.setupWithNavController(navController)
+        binding.bottomNavigationMenu.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.fragment_menu -> {
+                    if (binding.additionalMenuCard.visibility == android.view.View.VISIBLE){
+                        binding.additionalMenuCard.visibility = android.view.View.GONE
+                    } else {
+                        binding.additionalMenuCard.visibility = android.view.View.VISIBLE
+                    }
+                    return@setOnItemSelectedListener true
+                }
+                else -> {
+                    navController.navigate(it.itemId)
+                }
+            }
+            return@setOnItemSelectedListener false
+        }
+        binding.fragmentContainer.setOnClickListener {
+            if (binding.additionalMenuCard.visibility == android.view.View.VISIBLE){
+                binding.additionalMenuCard.visibility = android.view.View.GONE
+            }
+        }
     }
 }
