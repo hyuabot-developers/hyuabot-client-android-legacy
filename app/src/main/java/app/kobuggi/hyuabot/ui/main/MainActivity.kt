669c5928014eff5dc73fe1dfba9d0a98ea820fd9
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-07-14 11:38:13 +0900

    fix: 추가 메뉴 UI를 통일성 있게 구성 (#12)
    * feat: 통일성 있는 하단 네비게이션 바로 수정
    
    * fix: Compile SDK 수정

--- app/src/main/java/app/kobuggi/hyuabot/ui/main/MainActivity.kt
+++ app/src/main/java/app/kobuggi/hyuabot/ui/main/MainActivity.kt
@@ -1,10 +1,9 @@
 package app.kobuggi.hyuabot.ui.main
 
+import android.view.View
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
 import app.kobuggi.hyuabot.GlobalActivity
 import app.kobuggi.hyuabot.R
 import app.kobuggi.hyuabot.databinding.ActivityMainBinding
@@ -15,7 +14,7 @@ import app.kobuggi.hyuabot.ui.main.subway.SubwayFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : GlobalActivity() {
+class MainActivity : GlobalActivity(), View.OnClickListener {
     private val vm by viewModels<MainViewModel>()
     private lateinit var binding: ActivityMainBinding
 
@@ -25,35 +24,34 @@ class MainActivity : GlobalActivity() {
         binding.lifecycleOwner = this
         binding.vm = vm
 
-        val navigationHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
-        val navController = navigationHostFragment.navController
-
-        binding.bottomNavigationMenu.setupWithNavController(navController)
-        binding.bottomNavigationMenu.setOnItemSelectedListener {
-            if(it.itemId == R.id.fragment_menu) {
-                if (binding.additionalMenuCard.visibility == android.view.View.VISIBLE) {
-                    binding.additionalMenuCard.visibility = android.view.View.GONE
-                } else {
-                    binding.additionalMenuCard.visibility = android.view.View.VISIBLE
-                }
-                return@setOnItemSelectedListener false
+        binding.menuButton.setOnClickListener {
+            if(binding.additionalMenu.visibility == View.GONE){
+                binding.additionalMenu.visibility = View.VISIBLE
             } else {
-                binding.additionalMenuCard.visibility = android.view.View.GONE
-                val fragment = when(it.itemId){
-                    R.id.fragment_shuttle -> ShuttleFragment()
-                    R.id.fragment_bus -> BusFragment()
-                    R.id.fragment_subway -> SubwayFragment()
-                    R.id.fragment_cafeteria -> CafeteriaFragment()
-                    else -> throw IllegalArgumentException("Unknown fragment id")
-                }
-                openFragment(fragment)
-                return@setOnItemSelectedListener true
+                binding.additionalMenu.visibility = View.GONE
             }
         }
-        binding.fragmentContainer.setOnClickListener {
-            if (binding.additionalMenuCard.visibility == android.view.View.VISIBLE){
-                binding.additionalMenuCard.visibility = android.view.View.GONE
-            }
+
+        val buttonList = listOf(
+            binding.shuttleButton, binding.subwayButton, binding.cafeteriaButton, binding.busButton,
+            binding.readingRoomButton, binding.mapButton, binding.contactButton, binding.calendarButton,
+            binding.settingButton
+        )
+        buttonList.forEach{button -> button.setOnClickListener(this)}
+    }
+
+
+    override fun onClick(view: View) {
+        val fragment = when(view.id){
+            R.id.shuttle_button -> ShuttleFragment()
+            R.id.bus_button -> BusFragment()
+            R.id.subway_button -> SubwayFragment()
+            R.id.cafeteria_button -> CafeteriaFragment()
+            else -> null
+        }
+
+        if (fragment != null) {
+            openFragment(fragment)
         }
     }
 
