399df79a81a2efcadd07578b3badf77e055744bf
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-07-14 15:51:41 +0900

    feat: 셔틀버스 시간표 제목을 변수로 설정할 수 있도록 수정

--- app/src/main/java/app/kobuggi/hyuabot/ui/main/shuttle/timetable/ShuttleTimetableFragment.kt
+++ app/src/main/java/app/kobuggi/hyuabot/ui/main/shuttle/timetable/ShuttleTimetableFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import app.kobuggi.hyuabot.R
 import app.kobuggi.hyuabot.databinding.FragmentShuttleTimetableBinding
 
 
@@ -21,5 +23,25 @@ class ShuttleTimetableFragment: Fragment() {
         binding.lifecycleOwner = this
         Log.d("ShuttleTimetableFragment", "onCreateView")
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val args: ShuttleTimetableFragmentArgs by navArgs()
+        val context = requireContext()
+        val item: ShuttleTimetable = args.shuttleTimetableItem
+
+        val shuttleTypeID = when(item.shuttleType){
+            "DH" -> R.string.shuttle_type_DH
+            "DY" -> R.string.shuttle_type_DY
+            "C" -> R.string.shuttle_type_C
+            else -> R.string.shuttle_type_C
+        }
+        binding.shuttleTimetableToolbar.title = context.getString(
+            R.string.shuttle_timetable_toolbar,
+            context.getString(item.stopID),
+            context.getString(shuttleTypeID)
+        )
+        Log.d("ShuttleTimetableFragment", binding.shuttleTimetableToolbar.title.toString())
     }
 }
