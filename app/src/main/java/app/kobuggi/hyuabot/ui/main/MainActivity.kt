9ea9934758cba0623bbbe89935c663db800cf310
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-07-15 15:06:41 +0900

    revert: 하단 네비게이션 롤백 (#13)

--- app/src/main/java/app/kobuggi/hyuabot/ui/main/MainActivity.kt
+++ app/src/main/java/app/kobuggi/hyuabot/ui/main/MainActivity.kt
@@ -4,6 +4,8 @@ import android.view.View
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import app.kobuggi.hyuabot.GlobalActivity
 import app.kobuggi.hyuabot.R
 import app.kobuggi.hyuabot.databinding.ActivityMainBinding
@@ -14,7 +16,7 @@ import app.kobuggi.hyuabot.ui.main.subway.SubwayFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : GlobalActivity(), View.OnClickListener {
+class MainActivity : GlobalActivity() {
     private val vm by viewModels<MainViewModel>()
     private lateinit var binding: ActivityMainBinding
 
@@ -24,48 +26,8 @@ class MainActivity : GlobalActivity(), View.OnClickListener {
         binding.lifecycleOwner = this
         binding.vm = vm
 
-        binding.menuButton.setOnClickListener {
-            if(binding.additionalMenu.visibility == View.GONE){
-                binding.additionalMenu.visibility = View.VISIBLE
-            } else {
-                binding.additionalMenu.visibility = View.GONE
-            }
-        }
-
-        val buttonList = listOf(
-            binding.shuttleButton, binding.subwayButton, binding.cafeteriaButton, binding.busButton,
-            binding.readingRoomButton, binding.mapButton, binding.contactButton, binding.calendarButton,
-            binding.settingButton
-        )
-        buttonList.forEach{button -> button.setOnClickListener(this)}
-    }
-
-
-    override fun onClick(view: View) {
-        val fragment = when(view.id){
-            R.id.shuttle_button -> ShuttleFragment()
-            R.id.bus_button -> BusFragment()
-            R.id.subway_button -> SubwayFragment()
-            R.id.cafeteria_button -> CafeteriaFragment()
-            else -> null
-        }
-
-        if (fragment != null) {
-            openFragment(fragment)
-        }
-    }
-
-    private fun openFragment(fragment: Fragment) {
-        if (supportFragmentManager.fragments.none { it::class == fragment::class }) {
-            supportFragmentManager.beginTransaction()
-                .add(R.id.fragment_container, fragment)
-                .commit()
-        } else {
-            supportFragmentManager.fragments.first { it::class == fragment::class }.also {
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.fragment_container, it)
-                    .commit()
-            }
-        }
+        val navigationFragmentHost = supportFragmentManager.findFragmentById(R.id.fragment_container) as? NavHostFragment
+        val navController = navigationFragmentHost?.navController ?: return
+        binding.bottomNavigationMenu.setupWithNavController(navController)
     }
 }
