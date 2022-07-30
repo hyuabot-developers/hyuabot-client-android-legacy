590352c0b236a94a62e5f61a8c7667bd326e7f0f
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-07-30 18:55:23 +0900

    feat: 애셋 delivery 적용

--- app/src/main/java/app/kobuggi/hyuabot/ui/MainActivity.kt
+++ app/src/main/java/app/kobuggi/hyuabot/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package app.kobuggi.hyuabot.ui
 
 import android.content.DialogInterface
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
@@ -9,13 +10,20 @@ import androidx.navigation.ui.setupWithNavController
 import app.kobuggi.hyuabot.GlobalActivity
 import app.kobuggi.hyuabot.R
 import app.kobuggi.hyuabot.databinding.ActivityMainBinding
+import com.google.android.play.core.assetpacks.AssetPackManager
+import com.google.android.play.core.assetpacks.AssetPackManagerFactory
+import com.google.android.play.core.assetpacks.AssetPackStateUpdateListener
+import com.google.android.play.core.assetpacks.model.AssetPackStatus
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : GlobalActivity(), DialogInterface.OnDismissListener {
     private val vm by viewModels<MainViewModel>()
     private lateinit var binding: ActivityMainBinding
     lateinit var navController: NavController
+    private lateinit var assetPackManager: AssetPackManager
+    private val fastFollowAssetPack = "fast_follow_pack"
 
     override fun onCreate(savedInstanceState: android.os.Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,9 +34,63 @@ class MainActivity : GlobalActivity(), DialogInterface.OnDismissListener {
         val navigationFragmentHost = supportFragmentManager.findFragmentById(R.id.fragment_container) as? NavHostFragment
         navController = navigationFragmentHost?.navController ?: return
         binding.bottomNavigationMenu.setupWithNavController(navController)
+        initAssetPackManager()
     }
 
     override fun onDismiss(dialogInterface: DialogInterface?) {
         recreate()
+    }
+
+    private fun initAssetPackManager() {
+        assetPackManager = AssetPackManagerFactory.getInstance(applicationContext)
+        registerListener()
+    }
+
+    private fun getAbsolutePath(assetPackName: String, relativeAssetPath: String): String? {
+        val assetPackPath = assetPackManager.getPackLocation(assetPackName) ?: return null
+
+        val assetsFolderPath = assetPackPath.assetsPath()
+        return "$assetsFolderPath/$relativeAssetPath"
+    }
+
+    private fun registerListener(){
+        val fastFollowAssetPackPath = getAbsolutePath(fastFollowAssetPack, "")
+        if (fastFollowAssetPackPath == null){
+            assetPackManager.registerListener(assetPackStateUpdateListener)
+            val assetPackList = arrayListOf<String>()
+            assetPackList.add(fastFollowAssetPack)
+            assetPackManager.fetch(assetPackList)
+        } else {
+            initFastFollow()
+        }
+    }
+
+    private val assetPackStateUpdateListener =
+        AssetPackStateUpdateListener { state ->
+            when(state.status()){
+                AssetPackStatus.PENDING -> {
+                    Log.i("AssetPackManager", "PENDING")
+                }
+                AssetPackStatus.DOWNLOADING -> {
+                    Log.i("AssetPackManager", "DOWNLOADING")
+                }
+                AssetPackStatus.COMPLETED -> {
+                    Log.i("AssetPackManager", "INSTALLED")
+                    initFastFollow()
+                }
+                else -> {
+                    Log.i("AssetPackManager", "UNKNOWN")
+                }
+            }
+        }
+
+    private fun initFastFollow() {
+        val assetsPath = getAbsolutePath(fastFollowAssetPack, "")
+        // 데이터베이스 업데이트
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        assetPackManager.unregisterListener(assetPackStateUpdateListener)
     }
 }
