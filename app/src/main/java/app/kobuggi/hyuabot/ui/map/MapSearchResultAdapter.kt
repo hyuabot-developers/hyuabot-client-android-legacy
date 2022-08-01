7b475651c4895c439517594c975027ec8ecfff0c
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-08-01 19:01:29 +0900

    feat: 검색 결과의 분류를 string.xml로 분리

--- app/src/main/java/app/kobuggi/hyuabot/ui/map/MapSearchResultAdapter.kt
+++ app/src/main/java/app/kobuggi/hyuabot/ui/map/MapSearchResultAdapter.kt
@@ -10,6 +10,24 @@ import app.kobuggi.hyuabot.data.database.AppDatabaseItem
 import app.kobuggi.hyuabot.databinding.ItemMapSearchBinding
 
 class MapSearchResultAdapter(private val context: Context, private var result: List<AppDatabaseItem>, val onClickItem: (item: AppDatabaseItem) -> Unit) : RecyclerView.Adapter<MapSearchResultAdapter.MapSearchResultViewHolder>() {
+    private val categoryStringID = hashMapOf(
+        "on campus" to R.string.on_campus,
+        "fast food" to R.string.fast_food,
+        "korean" to R.string.food_korean,
+        "cafe" to R.string.cafe,
+        "vietnamese" to R.string.food_vietnamese,
+        "chinese" to R.string.food_chinese,
+        "chicken" to R.string.chicken,
+        "bakery" to R.string.bakery,
+        "western" to R.string.food_western,
+        "other food" to R.string.other_food,
+        "japanese" to R.string.food_japanese,
+        "pizza" to R.string.pizza,
+        "pub" to R.string.pub,
+        "meat" to R.string.meat,
+        "building" to R.string.building,
+    )
+
 
     inner class MapSearchResultViewHolder(private val binding: ItemMapSearchBinding) : RecyclerView.ViewHolder(binding.root){
         fun bind(position: Int) {
@@ -18,7 +36,7 @@ class MapSearchResultAdapter(private val context: Context, private var result: L
                 binding.searchResultCategory.visibility = ViewGroup.GONE
             } else {
                 binding.searchResultCategory.visibility = ViewGroup.VISIBLE
-                binding.searchResultCategory.text = result[position].category
+                binding.searchResultCategory.text = context.getString(categoryStringID[result[position].category]!!)
             }
             binding.searchResultItem.setOnClickListener {
                 onClickItem(result[position])
