3f9d82076539936c28b0ba435f0c4b4d36a4490e
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-08-17 13:49:36 +0900

    feat: 마지막 셔틀버스 정보가 잘리는 문제 수정

--- app/src/main/java/app/kobuggi/hyuabot/ui/shuttle/ShuttleArrivalListAdapter.kt
+++ app/src/main/java/app/kobuggi/hyuabot/ui/shuttle/ShuttleArrivalListAdapter.kt
@@ -63,13 +63,13 @@ class ShuttleArrivalListAdapter(private val context: Context,
                 shuttleTimetable.filter { it.shuttleType == "C" && LocalTime.parse(it.shuttleTime, formatter).plusMinutes(timeDelta[stopList[position].nameID]!![2].toLong()) > now }.map { LocalTime.parse(it.shuttleTime, formatter).plusMinutes(timeDelta[stopList[position].nameID]!![2].toLong()) }
             }
             Log.d("timetable", timetableByStopC.toString())
-            val shuttleDHAdapter = ShuttleArrivalTimeAdapter(context, now, timetableByStopDH.subList(0, if(timetableByStopDH.isNotEmpty()) minOf(if (binding.expandButton.isSelected) 5 else 2, timetableByStopDH.size - 1) else 0)){
+            val shuttleDHAdapter = ShuttleArrivalTimeAdapter(context, now, timetableByStopDH.subList(0, if(timetableByStopDH.isNotEmpty()) minOf(if (binding.expandButton.isSelected) 5 else 2, timetableByStopDH.size) else 0)){
                 onClickCard(stopList[position].nameID, "DH")
             }
-            val shuttleDYAdapter = ShuttleArrivalTimeAdapter(context, now, timetableByStopDY.subList(0, if(timetableByStopDY.isNotEmpty()) minOf(if (binding.expandButton.isSelected) 5 else 2, timetableByStopDY.size - 1) else 0)){
+            val shuttleDYAdapter = ShuttleArrivalTimeAdapter(context, now, timetableByStopDY.subList(0, if(timetableByStopDY.isNotEmpty()) minOf(if (binding.expandButton.isSelected) 5 else 2, timetableByStopDY.size) else 0)){
                 onClickCard(stopList[position].nameID, "DY")
             }
-            val shuttleCAdapter = ShuttleArrivalTimeAdapter(context, now, timetableByStopC.subList(0, if(timetableByStopC.isNotEmpty()) minOf(if (binding.expandButton.isSelected) 5 else 2, timetableByStopC.size - 1) else 0)){
+            val shuttleCAdapter = ShuttleArrivalTimeAdapter(context, now, timetableByStopC.subList(0, if(timetableByStopC.isNotEmpty()) minOf(if (binding.expandButton.isSelected) 5 else 2, timetableByStopC.size) else 0)){
                 onClickCard(stopList[position].nameID, "C")
             }
             binding.shuttleDHTime.adapter = shuttleDHAdapter
@@ -105,12 +105,11 @@ class ShuttleArrivalListAdapter(private val context: Context,
             }
             binding.expandButton.setOnClickListener {
                 binding.expandButton.isSelected = !binding.expandButton.isSelected
-                shuttleDHAdapter.setArrivalTimeList(timetableByStopDH.subList(0, if(timetableByStopDH.isNotEmpty()) minOf(if (binding.expandButton.isSelected) 5 else 2, timetableByStopDH.size - 1) else 0))
-                shuttleDYAdapter.setArrivalTimeList(timetableByStopDY.subList(0, if(timetableByStopDY.isNotEmpty()) minOf(if (binding.expandButton.isSelected) 5 else 2, timetableByStopDY.size - 1) else 0))
-                shuttleCAdapter.setArrivalTimeList(timetableByStopC.subList(0, if(timetableByStopC.isNotEmpty()) minOf(if (binding.expandButton.isSelected) 5 else 2, timetableByStopC.size - 1) else 0))
+                shuttleDHAdapter.setArrivalTimeList(timetableByStopDH.subList(0, if(timetableByStopDH.isNotEmpty()) minOf(if (binding.expandButton.isSelected) 5 else 2, timetableByStopDH.size) else 0))
+                shuttleDYAdapter.setArrivalTimeList(timetableByStopDY.subList(0, if(timetableByStopDY.isNotEmpty()) minOf(if (binding.expandButton.isSelected) 5 else 2, timetableByStopDY.size) else 0))
+                shuttleCAdapter.setArrivalTimeList(timetableByStopC.subList(0, if(timetableByStopC.isNotEmpty()) minOf(if (binding.expandButton.isSelected) 5 else 2, timetableByStopC.size) else 0))
             }
             binding.shuttleDH.setOnClickListener {
-                Log.d("shuttleDH", "clicked")
                 onClickCard(stopList[position].nameID, "DH")
             }
             binding.shuttleDY.setOnClickListener {
