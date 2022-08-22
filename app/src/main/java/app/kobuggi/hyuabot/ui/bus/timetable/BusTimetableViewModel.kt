512154ed5eccc7c266619f45c6e11986fcd698d8
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-08-22 16:21:34 +0900

    fix: 버스 시간표 갱신 실패시 빈 화면이 나오지 않게 수정

--- app/src/main/java/app/kobuggi/hyuabot/ui/bus/timetable/BusTimetableViewModel.kt
+++ app/src/main/java/app/kobuggi/hyuabot/ui/bus/timetable/BusTimetableViewModel.kt
@@ -4,7 +4,9 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import app.kobuggi.hyuabot.BusQuery
+import app.kobuggi.hyuabot.utils.Event
 import com.apollographql.apollo3.ApolloClient
+import com.apollographql.apollo3.exception.ApolloNetworkException
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -13,13 +15,21 @@ import javax.inject.Inject
 @HiltViewModel
 class BusTimetableViewModel @Inject constructor(private val client: ApolloClient): ViewModel() {
     val busTimetable : MutableLiveData<List<BusQuery.Timetable>> = MutableLiveData(arrayListOf())
+    val showErrorToast : MutableLiveData<Event<Boolean>> = MutableLiveData(Event(false))
     fun fetchBusTimetable(routeName: String) {
         viewModelScope.launch {
-            val result = client.query(
-                BusQuery(listOf("한양대게스트하우스", "한양대정문"), listOf(routeName), "")
-            ).execute()
-            if (result.data != null && result.data!!.bus.isNotEmpty()) {
-                busTimetable.value = result.data!!.bus[0].timetable
+            try {
+                val result = client.query(
+                    BusQuery(listOf("한양대게스트하우스", "한양대정문"), listOf(routeName), "")
+                ).execute()
+                if (result.data != null && result.data!!.bus.isNotEmpty()) {
+                    busTimetable.value = result.data!!.bus[0].timetable
+                } else {
+                    busTimetable.value = listOf()
+                }
+            } catch (e: ApolloNetworkException){
+                busTimetable.value = listOf()
+                showErrorToast.value = Event(true)
             }
         }
     }
